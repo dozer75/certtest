bebe6b2fe41109084aaa36994147619c8e0640f6
Rune Gulbrandsen <runeg@bibits.no>
2019-04-10 22:35:51 +0200

    Initial project

---
+++ certtest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace certtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X509Certificate2 cert = null;
+
+            try
+            {
+                cert = new X509Certificate2("test.pfx", "abc.123");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cert retrieval failed, see below for error");
+                Console.WriteLine(e);
+                return;
+            }
+
+            Console.WriteLine($"Hello {cert.FriendlyName}!");
+        }
+    }
+}
